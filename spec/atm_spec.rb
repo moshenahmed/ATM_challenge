81ab95a37092d954a389fc149e2b4d527c3f5880
Thomas Ochman <thomas@nocebo.se>
2016-01-25 13:18:04 +0000

    added more specs

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -5,4 +5,13 @@ describe Atm do
     it "respnds to #withdraw method" do
        expect(subject).to respond_to :withdraw
     end
+    
+    it "allows for withdraw of 5$" do
+       expect(subject.withdraw(5)).to eq true 
+    end
+    
+    xit "does not allow for withdraw of 6$" do
+       expect(subject.withdraw(6)).to eq false 
+    end
+    
 end
