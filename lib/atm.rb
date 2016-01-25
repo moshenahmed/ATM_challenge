e681974c0f37afa192c42fbc5b168cb350e5f824
Mazen <mmeezzoo26@me.com>
2016-01-25 14:46:36 +0100

    negative value done

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,6 +1,16 @@
 class Atm
 
   def withdraw(amount)
-    amount % 5 == 0
+    case
+    when negative_value(amount) then 'No negative numbers, please!'
+    else
+      amount % 5 == 0
+    end
   end
+
+ private
+ 
+ def negative_value(value)
+   value <= 0
+ end
 end
