934c7de1e182cd0d052d8f4d914b17a51cad8fe3
tochman <thomas@nocebo.se>
2016-01-25 16:37:41 +0100

    fixed typos in spec file

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,10 +2,10 @@ require './lib/atm.rb'
 
 describe Atm do
  # Check for validity if the card (exp date)
- # Check for card status :active or :deactivated 
+ # Check for card status :active or :deactivated
  # Subtract the withdrawal amount from ATM's availible funds
- # Handler for a case when user tries to withdra more then ATM is currently holding
- # Present user with info about what banknotes he recieves 
+ # Handler for a case when user tries to withdraw more then ATM is currently holding
+ # Present user with info about what banknotes he recieves
  # for example we are currently only holding 5$ bills [5, 5] = 10$, [5,5,5,5,5] = 25$
  # But in the future implementation we cam have 5, 10 & 20$ bills, then [5,20] = 25$
 
@@ -18,15 +18,15 @@ describe Atm do
        expect(subject.withdraw(5, 1234)).to eq true
     end
 
-    it "does not allow for withdraw of 5$ if oin is wrong" do
+    it "does not allow for withdraw of 5$ if pin is wrong" do
        expect(subject.withdraw(5, 4321)).to eq false
     end
 
-    it "does not allow for withdraw of 6$" do
+    it "does not allow for withdraw of 6$ even if pin is okay" do
        expect(subject.withdraw(6, 1234)).to eq false
     end
 
-    it "does not allow for withdraw of -10$" do
+    it "does not allow for withdraw of -10$ even if pin is okay" do
        expect(subject.withdraw(-10, 1234)).to eq 'No negative numbers, please!'
     end
   end
