4aa08242c2c27a749c351a9b7eb746572603f4b2
Thomas Ochman <thomas@nocebo.se>
2016-01-25 14:07:23 +0000

    added specs for #funds

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -3,7 +3,11 @@ require './lib/atm.rb'
 describe Atm do
 
 
-    
+ describe "ATM has sufficient funds" do
+     before do 
+        subject.funds = 1000     
+     end 
+     
     it "allows for withdraw of 5$ if pin is okay" do
        expect(subject.withdraw(5, 1234)).to eq true 
     end
@@ -19,5 +23,16 @@ describe Atm do
     it "does not allow for withdraw of -10$" do
        expect(subject.withdraw(-10, 1234)).to eq 'No negative numbers, please!'
     end
+  end
+  
+  describe "ATM is out of money" do
+     before do
+        subject.funds = 0 
+     end
+     
+    it "rejects withdraw of 5$ if pin is okay" do
+       expect(subject.withdraw(5, 1234)).to eq 'Sorry, withdrawal is not possible.' 
+    end
+  end
     
 end
