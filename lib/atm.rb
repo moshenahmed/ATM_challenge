c5d8c1b81a34f6a4bef31fb0f3557917ef79d622
Mazen <mmeezzoo26@me.com>
2016-01-25 15:22:58 +0100

    funds trial

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,19 +1,24 @@
 class Atm
+  att_accessor :funds
 
-  def withdraw(amount, pin)
+  def withdraw(amount, pin,)
     case
     when negative_value(amount) then 'No negative numbers, please!'
     else
-      amount % 5 == 0 and check_pin(pin)
+      case
+      when amount % 5 == 0 and check_pin(pin) @funds ==1000
+      else 
+
+      end
     end
   end
 
  private
- 
+
  def negative_value(value)
    value <= 0
  end
- 
+
  def check_pin(pin)
    pin == 1234
  end
