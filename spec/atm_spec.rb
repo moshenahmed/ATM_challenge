8407e17b295f3881d4a33e82d6941b3509b41636
Thomas Ochman <thomas@nocebo.se>
2016-01-25 13:31:10 +0000

    added spec about negative numbers

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -14,4 +14,8 @@ describe Atm do
        expect(subject.withdraw(6)).to eq false 
     end
     
+    it "does not allow for withdraw of -10$" do
+       expect(subject.withdraw(-10)).to eq 'No negative numbers, please!'
+    end
+    
 end
