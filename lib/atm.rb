f76d1800561b0926daee2e55117fe9332c317bfe
Mazen <mmeezzoo26@me.com>
2016-01-26 08:32:22 +0100

    subtracting trial

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,19 +1,30 @@
 class Atm
   attr_accessor :funds
   #INITIAL_AMOUNT_IN_ATM = 1000
-  
-  #def initialize
-  #  @funds = INITIAL_AMOUNT_IN_ATM
-  #end
+
+  def initialize
+    @funds = 1000
+  end
+
+  def remaining_available_funds(amount)
+    @@funds = @funds - amount
+  end
 
   def withdraw(amount, pin)
       case
-      when !sufficient_fund_in_atm then 'Sorry, withdrawal is not possible.'
-      when negative_value(amount) then 'No negative numbers, please!'
-      else
-        amount % 5 == 0 and check_pin(pin) 
+        when !sufficient_fund_in_atm then 'Sorry, withdrawal is not possible.'
+        when negative_value(amount) then 'No negative numbers, please!'
+       else
+         amount % 5 == 0 and check_pin(pin) #then  @@funds = @funds - amount
+         #update @@funds = @funds - amount
       end
+
+#  def remaining_available_funds(amount)
+#       each.withdraw
+#      end
   end
+
+
 
  private
 
@@ -24,7 +35,7 @@ class Atm
  def check_pin(pin)
    pin == 1234
  end
- 
+
  def sufficient_fund_in_atm
    @funds > 0
  end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,10 +2,10 @@ require './lib/atm.rb'
 
 describe Atm do
  # Check for validity if the card (exp date)
- # Check for card status :active or :deactivated 
+ # Check for card status :active or :deactivated
  # Subtract the withdrawal amount from ATM's availible funds
  # Handler for a case when user tries to withdra more then ATM is currently holding
- # Present user with info about what banknotes he recieves 
+ # Present user with info about what banknotes he recieves
  # for example we are currently only holding 5$ bills [5, 5] = 10$, [5,5,5,5,5] = 25$
  # But in the future implementation we cam have 5, 10 & 20$ bills, then [5,20] = 25$
 
@@ -29,9 +29,13 @@ describe Atm do
     it "does not allow for withdraw of -10$" do
        expect(subject.withdraw(-10, 1234)).to eq 'No negative numbers, please!'
     end
+
+    it "subtract withdrawal amount from avilable funds" do
+       expect(remaining_available_funds(5, 1234)).to eq 995
+    end
   end
 
-  describe "ATM is out of money" do
+describe "ATM is out of money" do
      before do
         subject.funds = 0
      end
